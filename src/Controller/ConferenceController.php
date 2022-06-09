d9d6b48cdaadb87da4625346f06132fc5af5d228
davidbermudez <davidbermudez@jerez.es>
2022-06-09 19:02:43 +0000

    Creando un paginador

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -7,6 +7,7 @@ use App\Repository\ConferenceRepository;
 use App\Repository\CommentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
@@ -16,8 +17,9 @@ class ConferenceController extends AbstractController
      */
     public function index(ConferenceRepository $conferenceRepository): Response
     {
+        
         return $this->render('conference/index.html.twig', [
-            'conferences' => $conferenceRepository->findAll(),
+            'conferences' => $conferenceRepository->findAll(),            
         ]);
         /*
         // MAINTENANCE
@@ -36,11 +38,15 @@ EOF
     /**
      * @Route("/conference/{id}", name="conference")
      */
-    public function show(Conference $conference, CommentRepository $commentRepository): Response
+    public function show(Request $request, Conference $conference, CommentRepository $commentRepository): Response
     {
+        $offset = max(0, $request->query->getInt('offset', 0));
+        $paginator = $commentRepository->getCommentPaginator($conference, $offset);
         return $this->render('conference/show.html.twig', [
             'conference' => $conference,
-            'comments' => $commentRepository->findBy(['conference' => $conference], ['createdAt' => 'DESC']),
+            'comments' => $paginator,
+            'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
+            'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
         ]);
     }
 }
